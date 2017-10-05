de6a7783d84357f2c03da1e4a94ac87b4a937419
LARS BERG <larsgbr@gmail.com>
2017-10-04 20:58:54 -0500

    Made the comments easier to understand

--- earthquakes.py
+++ earthquakes.py
@@ -1,11 +1,11 @@
 
-#enables us to open the url to retrieve the information we need
+# module used to open the url to retrieve the information we need
 import urllib2
-#enables us to manipulte the json information
+# module used to manipulte the json information
 import json
 
 def printResults(info):
-  # Use the json module to load and process the information
+  # Takes the json module and loads and processes the information
   theEarthquakeInfo = json.loads(info)
 
   # displays earthquakes with magnidude of 4.5 or greater
@@ -15,16 +15,16 @@ def printResults(info):
 
 def main():
 
-  # create a variable to hold the source URL
-  # This is the feed shows all the earthquakes with 4.5 or greater magnitudes
+  # variable holds the source URL
+  # This feed gathers information on earthquakes with 4.5 or greater magnitudes
   urlInfo = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson"
 
   # Open the URL and read's the info
-  webInfo = urllib2.urlopen(urlInfo) #calls the urlopen function on the url to get the information
-  print (webInfo.getcode()) #prints out the result code to make sure we're getting a result code of 200
+  webInfo = urllib2.urlopen(urlInfo) # calls the urlopen function on the url to get the requested information
+  print (webInfo.getcode()) # prints out the result code to make sure we're getting a result code of 200
   if (webInfo.getcode() == 200):
-    info = webInfo.read()     #if the code comes back as 200 then get the json info from the website
-    printResults(info) # prints out the results
+    info = webInfo.read()     # if the code comes back as 200 then get the json info from the website
+    printResults(info) # executes the function from above to display the earthquake information
   else:
     print "Houston we have a problem. I dunno what it is yet but we have a problem" + str(webInfo.getcode())
 
